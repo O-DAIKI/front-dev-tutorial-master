99499ed2dcff9bd6337449c9b5ed76838f4b3f51
O-DAIKI <mesh2.s.m.k@gmail.com>
2021-01-14 20:43:10 +0900

    修正

--- 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
+++ 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
@@ -3,16 +3,16 @@ const person = {
   age: 32,
   gender: 'male',
   interests: {
-    sports: 'soccer',
-    music: 'piano'
+      sports: 'soccer',
+      music: 'piano'
   },
-  getFullName:  function() {
-    console.log(this.name[0] + this.name[1])
+  getFullName: function() {
+      console.log(this.name[0] + this.name[1]) 
   }
 };
 
 const ageKey = 'age';
-person[ageKey] = 15;
+person[ageKey] = 12;
 console.log(person.age);
 person.getFullName();
 // this は宣言された際のオブジェクトを指している
