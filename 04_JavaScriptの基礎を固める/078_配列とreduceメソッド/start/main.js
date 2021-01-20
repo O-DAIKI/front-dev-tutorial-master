b509f0332208c004198b0b629f013425c7e1297d
O-DAIKI <mesh2.s.m.k@gmail.com>
2021-01-20 19:55:09 +0900

    Update main.js

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,12 +1,9 @@
 const arry = [1,2,3,4,5];
 
-arry.reduce(
-  function(accu, curr) {
-  console.log(accu, curr);
-  // return accu * curr;
+arry.reduce(function(accu, curr) {
+    console.log(accu, curr);
+    // return accu * curr;
 });
-
-// accu = accumulate(蓄積する)
 
 const str = 'animation';
 const strArry = str.split('');
@@ -14,7 +11,7 @@ const strArry = str.split('');
 console.log(strArry);
 
 const result = strArry.reduce((accu, curr) => {
-  return `${accu}<${curr}>`;
+    return accu + '<' + curr + '>';
 }, "")
 
 console.log(result);
