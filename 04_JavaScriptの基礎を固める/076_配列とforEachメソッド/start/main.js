944bc3df22db765416f7bc75e9b2ccae9fdb1dea
O-DAIKI <mesh2.s.m.k@gmail.com>
2021-01-21 23:06:44 +0900

    修正

--- 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
@@ -1,10 +1,14 @@
 const arry = [1,2,3,4,5];
 
-arry.forEach(v => console.log(v));
+arry.forEach(function(v, i, ary) {
+    console.log(v);
+});
+
+// arry.forEach(v => console.log(v));
 
 for(let i = 0; i < arry.length; i++) {
-  const val = arry[i];
-  console.log(val);
+    const v = arry[i];
+    console.log(v);
 }
 
 // forEachメソッドでは第一引数に配列の値,第二引数に添字,第三引数に配列が渡ってくる。
